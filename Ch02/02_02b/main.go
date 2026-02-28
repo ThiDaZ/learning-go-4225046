0a4d5c5c1b52a530ba99d7864ae2e600788cc716
Thidas Wickramarachchi <69147484+ThiDaZ@users.noreply.github.com>
2026-02-28 18:20:57 +0000

    setting varibles

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,15 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
 
-	fmt.Println("Hello from Go!")
+	aNumber := 12
+
+	fmt.Println(str1, str2, str3)
+	stringLength, err := fmt.Println("The value is", aNumber)
+	if err == nil {
+		fmt.Println("The string length is", stringLength)
+	}
 }
