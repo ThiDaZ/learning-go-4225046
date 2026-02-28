4cac3552ca75ddba8a198f362ed269fef02457de
Thidas Wickramarachchi <69147484+ThiDaZ@users.noreply.github.com>
2026-02-28 18:28:16 +0000

    added console text reader

--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter text: ")
+	str, _ := reader.ReadString('\n')
+	fmt.Println("Input text:", str)
 }
