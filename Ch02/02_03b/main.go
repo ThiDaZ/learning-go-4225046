65a1b690d9e5e32ff5e34ef2ae0d3fefd3f4faca
Thidas Wickramarachchi <69147484+ThiDaZ@users.noreply.github.com>
2026-03-01 08:44:03 +0000

    added number print fun

--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -4,11 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter text: ")
+	fmt.Print("Enter text: ")
 	str, _ := reader.ReadString('\n')
 	fmt.Println("Input text:", str)
+
+	fmt.Print("Enter number: ")
+	str, _ = reader.ReadString('\n')
+	f, err := strconv.ParseFloat(strings.TrimSpace(str), 64)
+	
+	if err != nil {
+		fmt.Println(err)
+	}else{
+		fmt.Println("Input value: ", f)
+	}
+	
 }
