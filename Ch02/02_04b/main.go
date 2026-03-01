0b4c098c45b3af2686ec9a2d4d452929a045290f
Thidas Wickramarachchi <69147484+ThiDaZ@users.noreply.github.com>
2026-03-01 08:53:22 +0000

    added math operations

--- Ch02/02_04b/main.go
+++ Ch02/02_04b/main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Math")
+
+	i1, i2, i3 := 12 , 14, 16
+	intSum := i1 + i2 + i3
+	fmt.Println("Integer sum:", intSum)
+
+	f1, f2, f3 := 12.5, 15.2, 4.9
+	floatSum := f1 + f2 + f3
+	fmt.Println("Float sum:", floatSum)
+
+	total := floatSum + float64(intSum)
+	fmt.Println("Total:", total)
+
+	sub := float64(intSum) * floatSum
+	fmt.Println("Value:", sub)
+
 }
